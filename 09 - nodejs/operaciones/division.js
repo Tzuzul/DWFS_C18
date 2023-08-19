fdbae0308af783405315958ae4ab4a3753141a0a
Tzuzul Rosas <tzuzulrosas@gmail.com>
2023-08-19 13:12:39 -0600

    Despliegue

--- 09 - nodejs/operaciones/division.js
+++
@@ -1,3 +0,0 @@
-const division =(a,b)=>a/b
-
-module.exports = division
--- 13 - react/segundo-proyecto/src/components/InputColor.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Form from 'react-bootstrap/Form';
-
-export default function InputColor(props) {
-    console.log(props)
-  return <Form.Control type="color" value={props.value} onChange={props.onChange}/>
-}
